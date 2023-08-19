b09f8eab6f179902271e7250a116d253d29e3714
TrinhDucTiep <nhungthisope123@gmail.com>
2023-08-19 14:02:26 +0700

    use 2 ways: annotation & xml to map servlets to urlpatterns

---
+++ src/main/java/com/example/cc/XMLServlet.java
@@ -0,0 +1,18 @@
+package com.example.cc;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+// this class show another way to map servlets to url pattern by using xml instead of annotation
+public class XMLServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Call Get in XML Servlet");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Call Post in XML Servlet");
+    }
+}
